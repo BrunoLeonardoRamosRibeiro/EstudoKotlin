941dbfeece4a8c36cef1f0fdec6f29d6bc6d8e02
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2024-06-11 19:31:11 -0300

    Frases do Dia

---
+++ AulaFrasesDoDia/app/src/main/java/com/louzeiroribeiro/aulafrasesdodia/MainActivity.java
@@ -0,0 +1,56 @@
+package com.louzeiroribeiro.aulafrasesdodia;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Atributo
+    String[] frases = {
+            "Frase 01", "Frase 02", "Frase 03",
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void gerarFrase(View view) {
+
+        TextView texto = findViewById(R.id.textResultado);
+
+        int numeroAleatorio = new Random().nextInt(3);
+
+        texto.setText(frases[numeroAleatorio]);
+
+    }
+
+    public void exibirTodas(View view) {
+
+        TextView texto = findViewById(R.id.textResultado);
+
+        String textoResultado = "";
+
+        for (String frase : frases) {
+            textoResultado = textoResultado + frase + "\n";
+        }
+
+        texto.setText(textoResultado);
+    }
+}
