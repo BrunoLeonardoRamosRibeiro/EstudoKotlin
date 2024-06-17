9b6a0102563ed2af9389e8ce2ce3de3ecd0b3ae9
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2024-06-17 19:25:36 -0300

    Escolha aleatória

--- AppPedraPapelTesoura/app/src/main/java/com/louzeiroribeiro/apppedrapapeltesoura/MainActivity.java
+++ AppPedraPapelTesoura/app/src/main/java/com/louzeiroribeiro/apppedrapapeltesoura/MainActivity.java
@@ -1,12 +1,16 @@
 package com.louzeiroribeiro.apppedrapapeltesoura;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +24,48 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void selecionarPedra(View view) {
+        verificarGanhador("pedra");
+    }
+
+    public void selecionarPapel(View view) {
+        verificarGanhador("papel");
+    }
+
+    public void selecionarTesoura(View view) {
+        verificarGanhador("tesoura");
+    }
+
+    private void verificarGanhador(String escolhaUsuario) {
+
+        String escolhaApp = geraEscolhaAleatoriaApp();
+
+    }
+
+    private String geraEscolhaAleatoriaApp() {
+        String[] opcoes = {"pedra", "papel", "tesoura"};
+
+        int numeroAleatorio = new Random().nextInt(3);
+
+        String escolhaApp = opcoes[numeroAleatorio];
+
+        ImageView imagemApp = findViewById(R.id.image_app);
+
+        switch (escolhaApp) {
+            case "pedra":
+                imagemApp.setImageResource(R.drawable.pedra);
+                break;
+            case "papel":
+                imagemApp.setImageResource(R.drawable.papel);
+                break;
+            case "tesoura":
+                imagemApp.setImageResource(R.drawable.tesoura);
+                break;
+
+        }
+
+        return escolhaApp;
     }
 }
