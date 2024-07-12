762a1bb270ac1f5ad2b14ca3bc6541c22e108389
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2024-07-12 11:12:48 -0300

    Aula ciclo de vida de uma activity

---
+++ AulaActivityFragment/app/src/main/java/com/louzeiroribeiro/aulaactivityfragment/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.louzeiroribeiro.aulaactivityfragment
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var buttonAbrir: Button
+
+    lateinit var buttonFecharApp: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Log.i("ciclo vida", "onCreate")
+
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        buttonAbrir = findViewById(R.id.button_abrir)
+
+        buttonAbrir.setOnClickListener() {
+
+            val intent = Intent(
+                this,
+                DetalhesActivity::class.java
+            )
+
+            startActivity(intent)
+        }
+
+        buttonFecharApp = findViewById(R.id.button_fechar_app)
+
+        buttonFecharApp.setOnClickListener() {
+            finish()
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.i("ciclo vida", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.i("ciclo vida", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.i("ciclo vida", "onPause")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("ciclo vida", "onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        Log.i("ciclo vida", "onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("ciclo vida", "onDestroy")
+
+    }
+
+
+}
---
+++ playground/android/app/src/main/kotlin/com/louzeiroribeiro/playground/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.louzeiroribeiro.playground
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
