56adad4460f970358457bd5f4558f9791a2c09ed
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2024-07-13 12:09:34 -0300

    Passando dados entre  telas - Parcelable

--- AulaActivityFragment/app/src/main/java/com/louzeiroribeiro/aulaactivityfragment/DetalhesActivity.kt
+++ AulaActivityFragment/app/src/main/java/com/louzeiroribeiro/aulaactivityfragment/DetalhesActivity.kt
@@ -42,10 +42,12 @@ class DetalhesActivity : AppCompatActivity() {
 
 //
 
-            val filme = if (Build.VERSION.SDK_INT >= 33) {
-                bundle.getSerializable("filme", Filme::class.java)
+            val filme = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+//                bundle.getSerializable("filme", Filme::class.java)
+                bundle.getParcelable("filme", Filme::class.java)
             } else {
-                bundle.getSerializable("filme") as Filme
+//                bundle.getSerializable("filme") as Filme
+                bundle.getParcelable("filme")
             }
 
             textFilme.text = "${filme?.nome} - ${filme?.distribuidor}"
--- AulaActivityFragment/app/src/main/java/com/louzeiroribeiro/aulaactivityfragment/Filme.kt
+++ AulaActivityFragment/app/src/main/java/com/louzeiroribeiro/aulaactivityfragment/Filme.kt
@@ -1,11 +1,13 @@
 package com.louzeiroribeiro.aulaactivityfragment
 
-import java.io.Serializable
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Filme(
     val nome: String,
     val descricao: String,
     val avaliacoes: Double,
     val diretor: String,
     val distribuidor: String
-): Serializable
+): Parcelable
