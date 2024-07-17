bb03c97f3066485dcdadbdbde1dd1650367ffbab
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2024-07-17 19:29:13 -0300

    Funções lambda

---
+++ AulaComponentesListagemColecoes/app/src/main/java/com/louzeiroribeiro/aulacomponenteslistagemcolecoes/teste/Recursos.java
@@ -0,0 +1,11 @@
+package com.louzeiroribeiro.aulacomponenteslistagemcolecoes.teste;
+
+public class Recursos {
+
+
+    public static final class id {
+        public static final int text_numero = 1009;
+    }
+
+
+}
---
+++ AulaComponentesListagemColecoes/app/src/main/java/com/louzeiroribeiro/aulacomponenteslistagemcolecoes/teste/funcoes.kt
@@ -0,0 +1,48 @@
+package com.louzeiroribeiro.aulacomponenteslistagemcolecoes.teste
+
+class Matematica {
+}
+
+fun executar(){
+    println("executar")
+}
+
+/*class Botao {
+    fun configurarCliqueBotao( nome: String, funcao: () -> Unit ){
+        funcao()
+        println("Nome: $nome")
+    }
+}*/
+
+class Botao {
+    fun configurarCliqueBotao( funcao: (String) -> Unit ){
+        funcao("Bruno")
+    }
+}
+
+fun executarClique(){
+    println("Executar clique do botão função comum")
+}
+
+fun main(){
+
+    val botao = Botao()
+
+    botao.configurarCliqueBotao{
+        println("Executu função lambda $it")
+    }
+//    botao.configurarCliqueBotao {
+//        println("Executu função lambda")
+//    }
+
+/*    // Função lambda
+    var funcaoLambda = { nome: String, idade: Int ->
+        println("Executar nome: $nome idade: $idade")
+    }
+
+    funcaoLambda("Bruno" , 47)*/
+
+
+
+
+}
