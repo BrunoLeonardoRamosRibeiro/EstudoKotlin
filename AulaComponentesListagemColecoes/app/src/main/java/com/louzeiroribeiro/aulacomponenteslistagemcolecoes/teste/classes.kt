5fbccdb8c41251fd6dedad0d57c0bee616dc23ae
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2024-07-22 18:14:41 -0300

    getter e setters

--- AulaComponentesListagemColecoes/app/src/main/java/com/louzeiroribeiro/aulacomponenteslistagemcolecoes/teste/classes.kt
+++ AulaComponentesListagemColecoes/app/src/main/java/com/louzeiroribeiro/aulacomponenteslistagemcolecoes/teste/classes.kt
@@ -16,33 +16,71 @@ data class Pergunta(val pergunta: String, val respostaCerta: Int) {
 }
 
 
-fun main (){
-    /// Desestruturação
+// getter e setters
 
-    val pergunta1 = Pergunta ("Qual a pergunta?", 1)
-    val pergunta2 = Pergunta ("Qual a pergunta?", 2)
+class Usuario() {
 
-    val (pergunta, resposta ) = pergunta1
+    var nome: String = ""
+        get() {
+            println("get: $field")
+            return field.uppercase()
+        }
+        set(value) {
+            field = "set: $value"
+        }
 
-    println(pergunta)
-    println(resposta)
-
-
-/*
-    val motorista = Motorista("Bruno")
-    val caminhao = motorista.Caminhao("FH 6000")
-    caminhao.exibirDadosCaminhao()
-*/
-
+    var idade: Int = 0
+    val maiorIdade
+        get() = idade >= 18
 
 
-/*
-    val caminhao = Motorista.Caminhao("FH 600")
-*/
+}
 
-/*
-    val motorista = Motorista("Bruno")
-    motorista.exibirDadosMotorista()
-*/
+
+fun main() {
+
+    val usuario = Usuario()
+    usuario.nome = "Pedro"
+    usuario.idade = 21
+
+    println("Nome: ${usuario.nome} - Idade: ${usuario.idade} - Tem maioridade? ${usuario.maiorIdade}")
+
+    /*
+        val pessoa = Pessoa()
+        pessoa.nome = "Bruno"
+
+        println("Nome:  ${pessoa.nome}")
+
+    */
+
+
+    /*
+        /// Desestruturação
+
+        val pergunta1 = Pergunta ("Qual a pergunta?", 1)
+        val pergunta2 = Pergunta ("Qual a pergunta?", 2)
+
+        val (pergunta, resposta ) = pergunta1
+
+        println(pergunta)
+        println(resposta)
+    */
+
+
+    /*
+        val motorista = Motorista("Bruno")
+        val caminhao = motorista.Caminhao("FH 6000")
+        caminhao.exibirDadosCaminhao()
+    */
+
+
+    /*
+        val caminhao = Motorista.Caminhao("FH 600")
+    */
+
+    /*
+        val motorista = Motorista("Bruno")
+        motorista.exibirDadosMotorista()
+    */
 
 }
