83ff9abfe1067cf656c5a36bedcb2113405f126b
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2024-07-22 17:34:42 -0300

    data Class

--- AulaComponentesListagemColecoes/app/src/main/java/com/louzeiroribeiro/aulacomponenteslistagemcolecoes/teste/classes.kt
+++ AulaComponentesListagemColecoes/app/src/main/java/com/louzeiroribeiro/aulacomponenteslistagemcolecoes/teste/classes.kt
@@ -1,5 +1,6 @@
 package com.louzeiroribeiro.aulacomponenteslistagemcolecoes.teste
 
+/*
 class Motorista (val nome: String) {
     fun exibirDadosMotorista() = println("Motorista: $nome")
 
@@ -7,11 +8,28 @@ class Motorista (val nome: String) {
         fun exibirDadosCaminhao() = println("Caminhão: $nomeCaminhao - motorista: $nome")
     }
 }
+*/
+
+data class Pergunta(val pergunta: String, val respostaCerta: Int) {
+
+
+}
+
+
 fun main (){
 
+    val pergunta1 = Pergunta ("Qual a pergunta?", 1)
+    val pergunta2 = Pergunta ("Qual a pergunta?", 2)
+
+    println( pergunta1 == pergunta2 )
+
+
+
+/*
     val motorista = Motorista("Bruno")
     val caminhao = motorista.Caminhao("FH 6000")
     caminhao.exibirDadosCaminhao()
+*/
 
 
 
